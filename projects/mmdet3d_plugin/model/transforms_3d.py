37d1fbfdf650f70c39c2dabeabf2e852b747ce49
WinterCode <liuchao20200202@gmail.com>
2023-09-18 15:21:24 +0800

    add bevfusion 3d transform

--- projects/mmdet3d_plugin/model/transforms_3d.py
+++ projects/mmdet3d_plugin/model/transforms_3d.py
@@ -3,12 +3,12 @@ from typing import Any, Dict
 
 import numpy as np
 import torch
+import mmcv
 from mmcv.transforms import BaseTransform
 from PIL import Image
 
 from mmdet3d.datasets import GlobalRotScaleTrans
 from mmdet3d.registry import TRANSFORMS
-
 
 @TRANSFORMS.register_module()
 class ImageAug3D(BaseTransform):
@@ -105,15 +105,17 @@ class ImageAug3D(BaseTransform):
         data['img'] = new_imgs
         # update the calibration matrices
         data['img_aug_matrix'] = transforms
+        data['lidar2img'] = data['img_aug_matrix'] @ data['lidar2img']
         return data
 
 
+
 @TRANSFORMS.register_module()
-class BEVFusionRandomFlip3D:
+class MFFusionRandomFlip3D:
     """Compared with `RandomFlip3D`, this class directly records the lidar
     augmentation matrix in the `data`."""
 
-    def __call__(self, data: Dict[str, Any]) -> Dict[str, Any]:
+    def __call__(self, data: dict ) -> dict :
         flip_horizontal = np.random.choice([0, 1])
         flip_vertical = np.random.choice([0, 1])
 
@@ -137,14 +139,15 @@ class BEVFusionRandomFlip3D:
                 data['gt_masks_bev'] = data['gt_masks_bev'][:, ::-1, :].copy()
 
         if 'lidar_aug_matrix' not in data:
-            data['lidar_aug_matrix'] = np.eye(4)
+             data['lidar_aug_matrix'] = np.eye(4)
         data['lidar_aug_matrix'][:3, :] = rotation @ data[
-            'lidar_aug_matrix'][:3, :]
+             'lidar_aug_matrix'][:3, :]
+        data['lidar2img'] = np.matmul( data['lidar2img'], data['lidar_aug_matrix'] )
         return data
 
 
 @TRANSFORMS.register_module()
-class BEVFusionGlobalRotScaleTrans(GlobalRotScaleTrans):
+class MFFusionGlobalRotScaleTrans(GlobalRotScaleTrans):
     """Compared with `GlobalRotScaleTrans`, the augmentation order in this
     class is rotation, translation and scaling (RTS)."""
 
@@ -167,10 +170,10 @@ class BEVFusionGlobalRotScaleTrans(GlobalRotScaleTrans):
 
         if 'pcd_scale_factor' not in input_dict:
             self._random_scale(input_dict)
-        self._trans_bbox_points(input_dict)
         self._scale_bbox_points(input_dict)
+        self._trans_bbox_points(input_dict)
 
-        input_dict['transformation_3d_flow'].extend(['R', 'T', 'S'])
+        input_dict['transformation_3d_flow'].extend(['R', 'S', 'T'])
 
         lidar_augs = np.eye(4)
         lidar_augs[:3, :3] = input_dict['pcd_rotation'].T * input_dict[
@@ -182,9 +185,199 @@ class BEVFusionGlobalRotScaleTrans(GlobalRotScaleTrans):
             input_dict['lidar_aug_matrix'] = np.eye(4)
         input_dict[
             'lidar_aug_matrix'] = lidar_augs @ input_dict['lidar_aug_matrix']
-
+        input_dict['lidar2img'] = np.matmul( input_dict['lidar2img'],
+                                             input_dict['lidar_aug_matrix'] )
         return input_dict
 
+
+'''
+@TRANSFORMS.register_module()
+class PadMultiViewImage(BaseTransform):
+    """Pad the multi-view image.
+    There are two padding modes: (1) pad to a fixed size and (2) pad to the
+    minimum size that is divisible by some number.
+    Added keys are "pad_shape", "pad_fixed_size", "pad_size_divisor",
+    Args:
+        size (tuple, optional): Fixed padding size.
+        size_divisor (int, optional): The divisor of padded size.
+        pad_val (float, optional): Padding value, 0 by default.
+    """
+
+    def __init__(self, size=None, size_divisor=None, pad_val=0):
+        self.size = size
+        self.size_divisor = size_divisor
+        self.pad_val = pad_val
+        # only one of size and size_divisor should be valid
+        assert size is not None or size_divisor is not None
+        assert size is None or size_divisor is None
+
+    def _pad_img(self, results):
+        """Pad images according to ``self.size``."""
+        if self.size is not None:
+            padded_img = [mmcv.impad(
+                img, shape=self.size, pad_val=self.pad_val) for img in results['img']]
+        elif self.size_divisor is not None:
+            padded_img = [mmcv.impad_to_multiple(
+                img, self.size_divisor, pad_val=self.pad_val) for img in results['img']]
+        results['img'] = padded_img
+        results['img_shape'] = [img.shape for img in padded_img]
+        results['pad_shape'] = [img.shape for img in padded_img]
+        results['pad_fixed_size'] = self.size
+        results['pad_size_divisor'] = self.size_divisor
+
+    def transform(self, results):
+        """Call function to pad images, masks, semantic segmentation maps.
+        Args:
+            results (dict): Result dict from loading pipeline.
+        Returns:
+            dict: Updated result dict.
+        """
+        self._pad_img(results)
+        return results
+
+@TRANSFORMS.register_module()
+class NormalizeMultiviewImage(BaseTransform):
+    """Normalize the image.
+    Added key is "img_norm_cfg".
+    Args:
+        mean (sequence): Mean values of 3 channels.
+        std (sequence): Std values of 3 channels.
+        to_rgb (bool): Whether to convert the image from BGR to RGB,
+            default is true.
+    """
+
+    def __init__(self, mean, std, to_rgb=True):
+        self.mean = np.array(mean, dtype=np.float32)
+        self.std = np.array(std, dtype=np.float32)
+        self.to_rgb = to_rgb
+
+    def transform(self, results):
+        """Call function to normalize images.
+        Args:
+            results (dict): Result dict from loading pipeline.
+        Returns:
+            dict: Normalized results, 'img_norm_cfg' key is added into
+                result dict.
+        """
+        results['img'] = [mmcv.imnormalize(
+            img, self.mean, self.std, self.to_rgb) for img in results['img']]
+        results['img_norm_cfg'] = dict(
+            mean=self.mean, std=self.std, to_rgb=self.to_rgb)
+        return results
+
+
+@TRANSFORMS.register_module()
+class ResizeCropFlipImage(BaseTransform):
+    """Random resize, Crop and flip the image
+    Args:
+        size (tuple, optional): Fixed padding size.
+    """
+
+    def __init__(self, data_aug_conf=None, training=True):
+        self.data_aug_conf = data_aug_conf
+        self.training = training
+
+    def transform(self, results):
+        """Call function to pad images, masks, semantic segmentation maps.
+
+        Args:
+            results (dict): Result dict from loading pipeline.
+        Returns:
+            dict: Updated result dict.
+        """
+
+        imgs = results['img']
+        N = len(imgs)
+        new_imgs = []
+        resize, resize_dims, crop, flip, rotate = self._sample_augmentation()
+        results['lidar2cam'] = np.array(results['lidar2cam'])
+        for i in range(N):
+            intrinsic = np.array(results['cam2img'][i])
+            viewpad = np.eye(4)
+            viewpad[:intrinsic.shape[0], :intrinsic.shape[1]] = intrinsic
+            results['cam2img'][i] = viewpad
+            img = Image.fromarray(np.uint8(imgs[i]))
+            # augmentation (resize, crop, horizontal flip, rotate)
+            # different view use different aug (BEV Det)
+            img, ida_mat = self._img_transform(
+                img,
+                resize=resize,
+                resize_dims=resize_dims,
+                crop=crop,
+                flip=flip,
+                rotate=rotate,
+            )
+            new_imgs.append(np.array(img).astype(np.float32))
+            results['cam2img'][
+                i][:3, :3] = ida_mat @ results['cam2img'][i][:3, :3]
+
+        results['img'] = new_imgs
+
+        return results
+
+    def _get_rot(self, h):
+
+        return torch.Tensor([
+            [np.cos(h), np.sin(h)],
+            [-np.sin(h), np.cos(h)],
+        ])
+
+    def _img_transform(self, img, resize, resize_dims, crop, flip, rotate):
+        ida_rot = torch.eye(2)
+        ida_tran = torch.zeros(2)
+        # adjust image
+        img = img.resize(resize_dims)
+        img = img.crop(crop)
+        if flip:
+            img = img.transpose(method=Image.FLIP_LEFT_RIGHT)
+        img = img.rotate(rotate)
+
+        # post-homography transformation
+        ida_rot *= resize
+        ida_tran -= torch.Tensor(crop[:2])
+        if flip:
+            A = torch.Tensor([[-1, 0], [0, 1]])
+            b = torch.Tensor([crop[2] - crop[0], 0])
+            ida_rot = A.matmul(ida_rot)
+            ida_tran = A.matmul(ida_tran) + b
+        A = self._get_rot(rotate / 180 * np.pi)
+        b = torch.Tensor([crop[2] - crop[0], crop[3] - crop[1]]) / 2
+        b = A.matmul(-b) + b
+        ida_rot = A.matmul(ida_rot)
+        ida_tran = A.matmul(ida_tran) + b
+        ida_mat = torch.eye(3)
+        ida_mat[:2, :2] = ida_rot
+        ida_mat[:2, 2] = ida_tran
+        return img, ida_mat
+
+    def _sample_augmentation(self):
+        H, W = self.data_aug_conf['H'], self.data_aug_conf['W']
+        fH, fW = self.data_aug_conf['final_dim']
+        if self.training:
+            resize = np.random.uniform(*self.data_aug_conf['resize_lim'])
+            resize_dims = (int(W * resize), int(H * resize))
+            newW, newH = resize_dims
+            crop_h = int(
+                (1 - np.random.uniform(*self.data_aug_conf['bot_pct_lim'])) *
+                newH) - fH
+            crop_w = int(np.random.uniform(0, max(0, newW - fW)))
+            crop = (crop_w, crop_h, crop_w + fW, crop_h + fH)
+            flip = False
+            if self.data_aug_conf['rand_flip'] and np.random.choice([0, 1]):
+                flip = True
+            rotate = np.random.uniform(*self.data_aug_conf['rot_lim'])
+        else:
+            resize = max(fH / H, fW / W)
+            resize_dims = (int(W * resize), int(H * resize))
+            newW, newH = resize_dims
+            crop_h = int(
+                (1 - np.mean(self.data_aug_conf['bot_pct_lim'])) * newH) - fH
+            crop_w = int(max(0, newW - fW) / 2)
+            crop = (crop_w, crop_h, crop_w + fW, crop_h + fH)
+            flip = False
+            rotate = 0
+        return resize, resize_dims, crop, flip, rotate
+'''
 
 @TRANSFORMS.register_module()
 class GridMask(BaseTransform):
